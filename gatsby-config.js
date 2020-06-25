e7092d6ebbe7a77c3f03e5442dedb9762ff4f01a
kanndide <kanndide@posteo.net>
2020-06-25 16:13:26 -0400

    forgot to do this to being with. done css. doin some graphql.

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Frontend Masters Gatsby Workshop',
+        description: 'A site we built togehter during a full-day Frontend Masters Workshop'
+    },
+    plugins: ['gatsby-plugin-emotion'],
+};
